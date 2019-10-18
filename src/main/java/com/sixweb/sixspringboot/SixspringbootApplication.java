a03d80a8f683293f338fcefd47910e5fa34ac92d
suntianli <tl.sun@tourage.cn>
2019-10-18 11:09:37 +0800

    first commit

---
+++ src/main/java/com/sixweb/sixspringboot/SixspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.sixweb.sixspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SixspringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sixweb/sixspringboot/SixspringbootApplicationTests.java
@@ -0,0 +1,14 @@
+package com.sixweb.sixspringboot;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SixspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
